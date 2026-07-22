3a545f9bb61d9188e6a50f2ea9c564886499d8c9
avnisshsingh <avnisshsingh45@gmail.com>
2026-07-22 22:12:21 +0530

    Added Reports Module

--- ReportsService.js
+++ ReportsService.js
@@ -1,14 +1,3 @@
-function getReportsData(fromDate, toDate) {
-
-    return {
-
-        success: true
-
-    };
-
-}
-
-
 /**
  * ==========================================
  * Get Reports Data
@@ -16,16 +5,101 @@ function getReportsData(fromDate, toDate) {
  */
 function getReportsData(fromDate, toDate) {
 
-    return {
+    let orders = [];
 
-        success: true,
+    const sheet = getSheet(SHEETS.ORDERS);
 
-        todaySales: 0,
+    const data = sheet.getDataRange().getValues();
 
-        todayOrders: 0,
+    data.shift();
 
-        todayProfit: 0
+    let totalOrders = 0;
+    let totalSales = 0;
 
-    };
+    let todayOrders = 0;
+    let todaySales = 0;
+    let todayProfit = 0;
+
+    const today = Utilities.formatDate(
+        new Date(),
+        Session.getScriptTimeZone(),
+        "yyyy-MM-dd"
+    );
+
+    data.forEach(function (row) {
+
+        const orderDate = Utilities.formatDate(
+            new Date(row[1]),
+            Session.getScriptTimeZone(),
+            "yyyy-MM-dd"
+        );
+
+        const grandTotal = Number(row[10]) || 0;
+
+        // KPI
+        if (orderDate === today) {
+
+            todayOrders++;
+
+            todaySales += grandTotal;
+
+        }
+
+        // Date Range Summary
+        if (fromDate && toDate) {
+
+            if (orderDate >= fromDate && orderDate <= toDate) {
+
+                totalOrders++;
+
+                totalSales += grandTotal;
+
+                orders.push({
+
+                orderId: row[0],
+
+                orderDate: Utilities.formatDate(
+                
+                new Date(row[1]),
+              
+                Session.getScriptTimeZone(),
+              
+                "dd/MM/yyyy"
+    ),
+
+    customer: row[2],
+
+    mobile: row[3],
+
+    paymentMode: row[8],
+
+    paymentStatus: row[9],
+
+    grandTotal: grandTotal
+
+});
+            }
+
+        }
+
+    });
+
+   return {
+
+    success: true,
+
+    todaySales: todaySales,
+
+    todayOrders: todayOrders,
+
+    todayProfit: todayProfit,
+
+    totalOrders: totalOrders,
+
+    totalSales: totalSales,
+
+    orders: orders
+
+};
 
 }
